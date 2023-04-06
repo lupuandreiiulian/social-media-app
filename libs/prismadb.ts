7a1c3e455d283eb7bdcc7ac94ea585e1b02c5cf8
Nine <108584650+lupuandreiiulian@users.noreply.github.com>
2023-04-06 18:55:04 +0300

    Update prismadb.ts

--- libs/prismadb.ts
+++ libs/prismadb.ts
@@ -1,5 +1,11 @@
 import { PrismaClient } from "@prisma/client"
 
-const prisma = new PrismaClient()
+let prisma: PrismaClient | undefined
+
+if (process.env.NODE_ENV !== "production") {
+  prisma = new PrismaClient()
+} else {
+  prisma = globalThis.prisma || new PrismaClient()
+}
 
 export default prisma
